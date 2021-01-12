b0b9450121b0ea0a9e55370d6d5e5fd25755f072
JovanSl <42840226+JovanSl@users.noreply.github.com>
2021-01-12 17:02:58 +0100

    Delete fxServis directory

--- fxServis/src/application/config.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class config {
-	protected String dbHost="localhost";
-	protected String dbPort="3306";
-	protected String dbUser="admin";
-	protected String dbPass="0000";
-	protected String dbName="projekat";
-}
